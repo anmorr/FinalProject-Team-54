e951bb2345c4de5c686c27a166ea8ebf3ce6635b
anmorr <anmorr@seas.upenn.edu>
2020-08-09 09:58:33 -0400

    ParkingViolation data model

--- src/edu/upenn/cit594/data/ParkingViolation.java
+++ src/edu/upenn/cit594/data/ParkingViolation.java
@@ -1,5 +1,59 @@
 package edu.upenn.cit594.data;
-
+/**
+ * This is a class to model the Parking Violation Data
+ * @author anmorr and ryanng
+ *
+ */
 public class ParkingViolation {
+	
+	protected String timeStamp;
+	protected Double fineAssessed;
+	protected String description;
+	protected String vehicleID;
+	protected String vehicleState;
+	protected String violationID;
+	protected String zipCode;
+	
+	public ParkingViolation(String timeStamp, Double fineAssessed, String description, String vehicleID,
+		String vehicleState, String violationID, String zipCode) {
+		this.timeStamp = timeStamp;
+		this.fineAssessed = fineAssessed;
+		this.description = description;
+		this.vehicleID = vehicleID;
+		this.vehicleState = vehicleState;
+		this.violationID = violationID;
+		this.zipCode = zipCode;
+	}
+
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+
+	public Double getFineAssessed() {
+		return fineAssessed;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getVehicleID() {
+		return vehicleID;
+	}
+
+	public String getVehicleState() {
+		return vehicleState;
+	}
+
+	public String getViolationID() {
+		return violationID;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+	
+	
 
 }
+
