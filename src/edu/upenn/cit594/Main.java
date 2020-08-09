e7511643957812aecb642a7b10140cd9ba16a578
anmorr <anmorr@seas.upenn.edu>
2020-08-08 21:52:16 -0400

    Added logger to the Main class

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,5 +1,16 @@
 package edu.upenn.cit594;
 
+import edu.upenn.cit594.logging.Logger;
+
 public class Main {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Logger.initialize(args[4]);
+	}
 
 }
--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,5 +1,50 @@
 package edu.upenn.cit594.logging;
 
+
+import java.io.PrintStream;
+
 public class Logger {
+	
+	
+	private PrintStream out;
+	private static Logger instance;
+	
+	/**
+	 * 1. Private Constructor
+	 * @param filename
+	 */
+	private Logger(String filename) {
+		try {
+			out = new PrintStream(filename);
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	/**
+	 * 2. Initialize Singleton instance with filename
+	 * @param filename
+	 */
+	public static void initialize(String filename) {
+		Logger.instance = new Logger(filename);
+	}
+	
+	/**
+	 * 3. Singleton access method
+	 * @return
+	 */
+	public static Logger getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * 4. Singleton log method
+	 * @param msg1
+	 * @param msg2
+	 */
+	public void log(String msg1, String msg2) {
+		out.printf("%-20s %s\n", msg1, msg2);
+	}
 
 }
+
