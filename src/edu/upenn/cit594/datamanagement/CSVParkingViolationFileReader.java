6c687729eac294e91f759c98b8870063afe59c72
ryanng14 <62918920+ryanng14@users.noreply.github.com>
2020-08-11 00:29:42 -0400

    added parking readers

--- src/edu/upenn/cit594/datamanagement/CSVParkingViolationFileReader.java
+++ src/edu/upenn/cit594/datamanagement/CSVParkingViolationFileReader.java
@@ -1,25 +1,50 @@
 package edu.upenn.cit594.datamanagement;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 import edu.upenn.cit594.data.Data;
+import edu.upenn.cit594.data.ParkingViolation;
 
 public class CSVParkingViolationFileReader implements Reader {
-	
+
 	String filename;
-	
-	
 
 	public CSVParkingViolationFileReader(String filename) {
 		this.filename = filename;
 	}
 
-
-
 	@Override
 	public List<Data> read() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Data> parkingViolationList = new ArrayList<Data>();
+		Scanner fileReader = null;
+		try {
+			fileReader = new Scanner(new File(filename));
+			while (fileReader.hasNextLine()) {
+				String eachViolation = fileReader.nextLine();
+				String[] violationComponents = eachViolation.split(",");
+				String timeStamp = violationComponents[0];
+				double fineAssessed = Double.parseDouble(violationComponents[1]);
+				String description = violationComponents[2];
+				String vehicleID = violationComponents[3];
+				String vehicleState = violationComponents[4];
+				String violationID = violationComponents[5];
+				String zipCode = violationComponents[6];
+				parkingViolationList.add(new ParkingViolation(timeStamp, fineAssessed, description, vehicleID, vehicleState, violationID, zipCode));
+
+			}
+		} catch (Exception e) {
+			System.out.println("Invalid Parking Violtions CSV Input File");
+			System.exit(0);
+			throw new IllegalStateException(e);
+
+		} finally {
+			fileReader.close();
+		}
+		return parkingViolationList;
 	}
 
 }
--- src/edu/upenn/cit594/datamanagement/JSONParkingViolationFileReader.java
+++ src/edu/upenn/cit594/datamanagement/JSONParkingViolationFileReader.java
@@ -1,8 +1,16 @@
 package edu.upenn.cit594.datamanagement;
 
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import edu.upenn.cit594.data.Data;
+import edu.upenn.cit594.data.ParkingViolation;
 
 public class JSONParkingViolationFileReader implements Reader {
 
@@ -10,10 +18,45 @@ public class JSONParkingViolationFileReader implements Reader {
 	public JSONParkingViolationFileReader(String filename) {
 		this.filename = filename;
 	}
+	
+	
 	@Override
 	public List<Data> read() {
 		// TODO Auto-generated method stub
-		return null;
+		JSONParser parser = new JSONParser();
+		JSONArray parkingViolations;
+
+		
+		try {
+			parkingViolations = (JSONArray)parser.parse(new FileReader(filename));
+		} catch (Exception e) {
+			System.out.println("Invalid Parking Violations Input File");
+			System.exit(0);
+			throw new IllegalStateException(e);
+		}
+		
+		// user an iterator to iterate over each element of the array
+		Iterator iter = parkingViolations.iterator();
+	
+		
+		List<Data> violationList = new ArrayList<Data>();
+		
+		while (iter.hasNext()) {
+			JSONObject parkingViolation = (JSONObject) iter.next();
+			String timeStamp = (String) parkingViolation.get("date");
+			double fineAssessed = (double) parkingViolation.get("fine");
+			String description = (String) parkingViolation.get("violation");
+			String vehicleID = (String) parkingViolation.get("plate_id");
+			String vehicleState = (String) parkingViolation.get("state");
+			String violationID = (String) parkingViolation.get("ticket_number");
+			String zipCode = (String) parkingViolation.get("zip_code");
+			violationList.add(new ParkingViolation(timeStamp, fineAssessed, description, vehicleID, vehicleState, violationID, zipCode));
+		}
+		
+		
+		
+		return violationList;
 	}
 
+	
 }
