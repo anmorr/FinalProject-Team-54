90b51dfa80bc04b6203bc3a5922a9eb3256d9089
ryanng14 <62918920+ryanng14@users.noreply.github.com>
2020-08-18 21:09:47 -0400

    Update Main method to reduce dependency

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,7 +1,5 @@
 package edu.upenn.cit594;
 
-import java.io.File;
-
 import edu.upenn.cit594.data.ParkingViolation;
 import edu.upenn.cit594.data.Population;
 import edu.upenn.cit594.data.Property;
@@ -15,101 +13,44 @@ import edu.upenn.cit594.processor.Processor;
 import edu.upenn.cit594.ui.CommandLineUserInterface;
 
 public class Main {
-	
-	static String parkingViolationFileType = null;
-	static String parkingViolationFileName = null;
-	static String propertyValueFileName = null;
-	static String populationFileName = null;
-	static String logfileName = null;
-	
-	static Reader<ParkingViolation> parkingViolationReader;
-	static Reader<Property> propertyValueReader;
-	static Reader<Population> populationReader;
-	
-	static Processor processor;
-	
-	/**
-	 * Helper file that prints message 
-	 * on error with input files.
-	 * @param msg
-	 */
-	private static void usage(int msg) {
-		if(msg == 1) {
-			System.out.println("Usage: java Main [file type (json/text)] [filename] "
-					+ "[property value csv file] [population txt file] [logfile]");
-			System.exit(0);
-		}
-		if(msg == 2){
-			System.out.println("1 or more files is not readable!"); 
-			System.out.println("Please verify that files exist and have correct permissions!");
-			System.exit(0);
-		}
-	}
-	
-	
-	/**
-	 * Helper method that checks for file readability
-	 * and permissions.
-	 * @param filename
-	 * @return
-	 */
-	private static boolean checkFile(String filename) {
-		File f = new File(filename);
-		if(f.canRead() && f.exists()) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static void main(String[] args) {
 
+
+	public static void main(String[] args) {
 		
-		
-		if(args.length < 5) {
+		if(args.length != 5) {
 			System.out.println("Usage: Incorrect number of arguments submitted.");
 			System.exit(0);
 		}
 		
 		// Initialize the logfile
-		logfileName = args[4];
+		String logfileName = args[4];
 		Logger.initialize(logfileName);
 		Logger instance = Logger.getInstance();
 		
-		parkingViolationFileType = args[0].toLowerCase();
-		if(parkingViolationFileType.contentEquals("json")) {
-			parkingViolationFileName = args[1];
-			if(!checkFile(parkingViolationFileName)){
-				usage(2);
-			}
+		String parkingViolationFileType = args[0];
+		String parkingViolationFileName = args[1];
+		String propertyValueFileName = args[2];
+		String populationFileName = args[3];
+		
+		Reader<ParkingViolation> parkingViolationReader = null;
+		Reader<Property> propertyValueReader;
+		Reader<Population> populationReader;
+	
+		if(parkingViolationFileType.equals("json")) {
 			parkingViolationReader = new JSONParkingViolationFileReader(parkingViolationFileName);
-		} else if (parkingViolationFileType.contentEquals("csv")) {
-			parkingViolationFileName = args[1];
-			if(!checkFile(parkingViolationFileName)){
-				usage(2);
-			}
+		} else if (parkingViolationFileType.equals("csv")) {
 			parkingViolationReader = new CSVParkingViolationFileReader(parkingViolationFileName);
-		}else {
-			usage(1);
+		} else {
+			System.out.println("Invalid Input File");
+			System.exit(0);
 		}
 		
-		propertyValueFileName = args[2];
-		if(!checkFile(propertyValueFileName)) {
-			usage(2);
-		}
 		propertyValueReader = new CSVPropertyValueFileReader(propertyValueFileName);
-		
-		populationFileName = args[3];
-		if(!checkFile(populationFileName)) {
-			usage(2);
-		}
 		populationReader = new TSVPopulationFileReader(populationFileName);
-		
-		
-		
-		processor = new Processor(parkingViolationReader, propertyValueReader, populationReader);
-		
+		Processor processor = new Processor(parkingViolationReader, propertyValueReader, populationReader);
+				
 		CommandLineUserInterface ui = new CommandLineUserInterface(processor);
-		
+
 		String arguments = parkingViolationFileType + " " + parkingViolationFileName + " " + propertyValueFileName
 				+ " " + populationFileName + " " + logfileName;
 		instance.log(arguments);
